f2585e5fb194b69918f9e74b8356df7d3029224f
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-11-18 15:55:45 +0200

    Add files via upload

---
+++ Reservation.Domain/Dtos/ReservationDto/CreateReservationResponseDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.Domain.Dtos.ReservationDto
+{
+    public class CreateReservationResponseDto
+    {
+        public int ReservationId { get; set; }
+        public int InvoiceId { get; set; }
+        public int PaymentId { get; set; }
+        public string PaymentIframeUrl { get; set; } = string.Empty;
+
+        public string ConfirmationCode { get; set; }
+    }
+}
---
+++ Reservation.Domain/Dtos/Reviews/ReservationReviewsDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reservation.Domain.Dtos.Reviews
+{
+    public class ReservationReviewsDto
+    {
+        public string GuestName { get; set; }
+        public double? Rating { get; set; }           
+        public string Comment { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
