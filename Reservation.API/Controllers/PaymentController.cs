01c6400d96a8de8c1d80a1836c302f25af4ecd4b
Gaber-eg <133911723+Gaber-dev@users.noreply.github.com>
2025-11-18 15:44:58 +0200

    Add files via upload

---
+++ Reservation.API/Controllers/PaymentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Reservation.API.Managers.ReservationManager;
+using Reservation.Domain.Dtos.PaymobDto;
+
+namespace Reservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IReservationService _reservationService;
+
+        public PaymentController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+        [HttpPost("paymob-callback")]
+        [AllowAnonymous]
+        public async Task<IActionResult> PaymobCallback([FromBody] PaymobCallbackDto dto)  // ← [FromBody] مش [FromForm]
+        {
+            await _reservationService.HandlePaymobCallbackAsync(dto);
+            return Ok();
+        }
+    }
+}
